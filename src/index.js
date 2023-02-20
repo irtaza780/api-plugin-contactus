0308440218988104fa220677964ff63fca2303c3
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-02-20 11:04:23 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { createRequire } from "module";
+import importAsString from "@reactioncommerce/api-utils/importAsString.js";
+import resolvers from "./resolver/resolver.js";
+const schemas = importAsString("./schema/schema.graphql");
+const require = createRequire(import.meta.url);
+const pkg = require("../package.json");
+
+console.log("Schema here", schemas);
+/**
+ * @summary Import and call this function to add this plugin to your API.
+ * @param {Object} app The ReactionAPI instance
+ * @returns {undefined}
+ */
+export default async function register(app) {
+  await app.registerPlugin({
+    label: pkg.label,
+    name: pkg.name,
+    version: pkg.version,
+    collections: {
+      Transactions: {
+        name: "Transactions",
+        updatedAt: { type: Date, default: Date.now },
+        createdAt: { type: Date, default: Date.now },
+      },
+    },
+    graphQL: {
+      schemas: [schemas],
+      resolvers,
+    },
+  });
+}
